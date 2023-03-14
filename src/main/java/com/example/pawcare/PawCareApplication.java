6e5776f27874d54d56c37dfefc8fd373f67c851f
Mohamed Mokhtar Jaafar <mohamedmokhtar.jaafar@esprit.tn>
2023-03-14 22:21:56 +0100

    first commit

---
+++ src/main/java/com/example/pawcare/PawCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pawcare/PawCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
