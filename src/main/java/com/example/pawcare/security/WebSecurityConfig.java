0011526341dd62530261ac8a116afca471231898
bilel <bileljemai97@gmail.com>
2023-03-31 05:19:17 +0100

    Authentication

--- src/main/java/com/example/pawcare/security/WebSecurityConfig.java
+++ src/main/java/com/example/pawcare/security/WebSecurityConfig.java
@@ -55,10 +55,8 @@ public class WebSecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                //.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
