7b7a6ab8a1569569231870e6c12d62db94172f44
Mohamed Mokhtar Jaafar <mohamedmokhtar.jaafar@esprit.tn>
2023-05-07 11:53:10 +0100

    Revert "Merge pull request #9 from jmokhtar2022/cart-management"
    This reverts commit c88aeacb64747d2c25c52e8a05569c09cef1495f, reversing
    changes made to 3fff965e446c18f0c7f1f4c30ec4b171ee3968a0.

--- src/main/java/com/example/pawcare/services/email/MailSender.java
+++
@@ -1,6 +0,0 @@
-package com.example.pawcare.services.email;
-
-public interface MailSender  {
-    void sendEmail(String to,String subject, String email);
-
-}
--- src/main/java/com/example/pawcare/services/item/ItemReports/ItemReportServices.java
+++
@@ -1,5 +0,0 @@
-package com.example.pawcare.services.item.ItemReports;
-
-public interface ItemReportServices {
-    public void generateDailyRevenueReport() ;
-    }
